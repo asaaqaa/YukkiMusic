641eb62b8d6554711be8ec72fd1ccd850bf795d1
asaaqaa <134310077+asaaqaa@users.noreply.github.com>
2023-08-05 02:32:50 +0300

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -29,7 +29,7 @@ HELP_COMMAND = get_command("HELP_COMMAND")
 
 
 @app.on_message(
-    filters.command(HELP_COMMAND)
+    filters.command(["help", "مساعده", "اوامر"]) | filters.command(["اوامر","الاوامر","مساعده"],prefixes= ["/", "!","","#"])
     & filters.private
     & ~filters.edited
     & ~BANNED_USERS
